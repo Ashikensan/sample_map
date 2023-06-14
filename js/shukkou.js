35fbb24614e6ce423194ebcfa57cf822589deca7
Ashikensan <kenji_ashi@yahoo.co.jp>
2023-06-15 00:12:23 +0900

    追加

--- js/shukkou.js
+++ js/shukkou.js
@@ -1,1 +1,16 @@
 // alert(12);
+const request = require('request');
+const options = {
+    'method': 'POST',
+    'url': 'https://api.pdf.co/v1/pdf/convert/to/text',
+    'headers': {
+        'x-api-key': 'YOUR_API_KEY'
+    },
+    formData: {
+        'url': 'https://bytescout-com.s3-us-west-2.amazonaws.com/files/demo-files/cloud-api/pdf-to-text/sample.pdf'
+    }
+};
+request(options, function (error, response) { 
+    if (error) throw new Error(error);
+    console.log(response.body);
+});
